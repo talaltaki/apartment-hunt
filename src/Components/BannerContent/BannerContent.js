b1713d595de083dff37699d3ee8b8b4d201ef975
ahmedmusa995 <sayeedsayem8@gmail.com>
2020-11-17 21:45:41 +0600

    final commit

--- src/Components/BannerContent/BannerContent.js
+++ src/Components/BannerContent/BannerContent.js
@@ -1,25 +1,3 @@
-// import React from 'react';
-
-// const BannerContent = () => {
-//     return (
-//         <div className="">
-//           <h1 className="text-capitalize mb-4">FIND YOUR HOUSE RENT</h1>
-//           <form>
-//   <div class="form-row d-flex justify-content-center">
-//     <div class="col-9">
-//       <input type="text" class="form-control form-control-m" placeholder="First name"/>
-//     </div>
-//     <div class="col mb-1">
-//       <button  className="btn common-btn px-4 ">Search</button>
-//     </div>
-//   </div>
-// </form>
-//         </div>
-//     );
-// };
-
-// export default BannerContent;
-
 import React from 'react';
 
 const BannerContent = ({ title }) => {
@@ -34,7 +12,7 @@ const BannerContent = ({ title }) => {
         title ? ((<h1>Apartment</h1>)) : (<form>
           <div className="form-row d-flex justify-content-center">
             <div className="col-9">
-              <input type="text" className="form-control form-control-m" placeholder="First name" />
+              <input type="text" className="form-control form-control-m" placeholder="Search House Here" />
             </div>
             <div className="col mb-1">
               <button className="btn common-btn px-4 ">Search</button>
--- src/Components/HomeDetails/HomeDetails.js
+++ src/Components/HomeDetails/HomeDetails.js
@@ -7,6 +7,7 @@ import Room2 from './Rectangle 409.png';
 import Room3 from './Rectangle 408.png';
 import Room4 from './Rectangle 407.png';
 import Form from "./Form";
+import Footer from "../Home/Footer/Footer";
 
 const HomeDetails = () => {
   const { id } = useParams();
@@ -84,93 +85,9 @@ const HomeDetails = () => {
           </div>
         </div>
       </div>
+      <Footer />
     </React.Fragment>
   );
 };
 
 export default HomeDetails;
-
-
-// import React, { useEffect } from "react";
-// import { useParams } from "react-router-dom";
-// import Banner from "../Banner/Banner";
-// import Navbar from "../Navbar/Navbar";
-// // import Description from "./Description";
-// import Form from "./Form";
-// // import Images from "./Images";
-
-// const HomeDetails = () => {
-//   const { id } = useParams();
-
-//   const [details, setDetails] = React.useState();
-
-//   useEffect(() => {
-//     fetch(`http://localhost:9999/house-details/${id}`)
-//       .then(res => res.json())
-//       .then(data => {
-//         setDetails(data);
-//       })
-//   }, []);
-
-//   // const { title, price, bed, convertedThumbnail, bathroom, _id } = details;
-
-
-//   // useEffect(() => {
-//   //   fetch(`http://pure-inlet-20297.herokuapp.com/rent-house-collection`)
-//   //     .then(res => res.json())
-//   //     .then(data => {
-//   //       const singleHouse = data.find(item => id === item._id);
-//   //       if (singleHouse !== undefined) {
-//   //         setSingleHouseDetails(singleHouse);
-//   //         console.log(singleHouse, singleHouseDetails)
-//   //       }
-//   //     })
-//   // }, []);
-
-
-
-//   // useEffect(() => {
-//   //   if (details) {
-//   //     const singleHouse = details.find(item => id === item._id);
-//   //     setSingleHouseDetails(singleHouse)
-//   //   }
-//   // }, []);
-
-
-
-//   return (
-//     <React.Fragment>
-//       <Navbar />
-//       <Banner />
-//       <br /> <br />
-//       <div className="container d-flex">
-//         <div className="row">
-//           <div className="col-md-8 col-12">
-//             <div>
-//               {/* <Images /> */}
-//               {/* <img src={`data:image/jpeg;base64,${convertedThumbnail.img}`} alt="" /> */}
-//             </div>
-//             <div>
-//               <h1>Family Apartment Three</h1>
-//               <h4 className="text-secondary">
-//                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-//                 Necessitatibus explicabo eaque sit minima error, perspiciatis,
-//                 blanditiis in soluta numquam unde id molestias cumque. Architecto
-//                 reprehenderit saepe aliquid alias eaque ea! Lorem ipsum dolor sit
-//                 amet consectetur adipisicing elit. Tenetur deserunt at,
-//                 consequatur doloremque nobis perspiciatis rem omnis consequuntur
-//                 culpa reprehenderit, esse ipsam voluptatibus illum dicta enim
-//                 delectus iure voluptates. Quos?
-//             </h4>
-//             </div>
-//           </div>
-//           <div className="col-md-4 col-12">
-//             <Form />
-//           </div>
-//         </div>
-//       </div>
-//     </React.Fragment>
-//   );
-// };
-
-// export default HomeDetails;
